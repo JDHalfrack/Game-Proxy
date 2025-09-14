913b0d6914a5122a488581273a379a4b4d906bee
JDHalfrack <48894405+JDHalfrack@users.noreply.github.com>
2025-09-14 13:18:49 -0500

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,13 +1,14 @@
-export default async function handler(req, res) {
+module.exports = async (req, res) => {
   try {
-    const response = await fetch("https://pulsematch.xyz/index.php?league=nfl");
+    const response = await fetch("https://joshdixon.com/index.php?league=nfl");
     const html = await response.text();
 
     // Allow any site to fetch this (CORS)
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Content-Type", "text/html; charset=utf-8");
+
     res.status(200).send(html);
   } catch (err) {
     res.status(500).send("Error fetching source: " + err.toString());
   }
-}
+};
